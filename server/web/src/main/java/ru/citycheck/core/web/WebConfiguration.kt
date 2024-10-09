aaa89b6a17ab551f5288d14601b5f07435f8d135
Daza10samu <andreyhigher@gmail.com>
2024-10-09 20:06:27 +0300

    feat: update configurations

--- server/web/src/main/java/ru/citycheck/core/web/WebConfiguration.kt
+++ server/web/src/main/java/ru/citycheck/core/web/WebConfiguration.kt
@@ -1,13 +1,30 @@
 package ru.citycheck.core.web
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import ru.citycheck.core.application.ApplicationConfiguration
+import java.util.List
+
 
 @Configuration
 @ComponentScan
 @Import(ApplicationConfiguration::class)
 @EnableAutoConfiguration
-class WebConfiguration
+class WebConfiguration {
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val serverMain = Server()
+        serverMain.url = "https://city24.civiltechgroup.ru"
+        serverMain.description = "Main server"
+
+        val serverLocal = Server()
+        serverLocal.setUrl("http://localhost:8080")
+        serverLocal.description = "Local server"
+        return OpenAPI().servers(listOf(serverMain, serverLocal))
+    }
+}
